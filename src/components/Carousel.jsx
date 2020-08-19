f329c9621fb17670ae07779e960ce773eb070e77
danyel117 <dsaldarriaga@prevalentware.com>
2020-08-19 16:29:15 -0500

    agregar estilos a carrusel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../assets/styles/components/Carousel.scss';
+
+const Carousel = ({ children }) => {
+  return (
+    <section className='carousel'>
+      <div className='carousel__container'>{children}</div>
+    </section>
+  );
+};
+
+export default Carousel;
--- src/components/HelloWorld.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../assets/styles/App.scss';
-
-const HelloWorld = () => {
-  return <h1>HelloWorld</h1>;
-};
-
-export default HelloWorld;
