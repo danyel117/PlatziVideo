1b4609bae2179733a7adb53e5f97ff40a8b0123e
danyel117 <dsaldarriaga@prevalentware.com>
2020-08-19 18:02:08 -0500

    agregar comunicación con API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,37 +5,49 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
 
 const App = () => {
+  const videos = useInitialState('http://localhost:3000/initialState');
   return (
     <div className='App'>
       <Header />
       <Search />
+      {videos && (
+        // eslint-disable-next-line react/jsx-fragments
+        <React.Fragment>
+          {videos.mylist.length > 0 && (
+            <Categories title='Mi Lista'>
+              <Carousel>
+                {videos.mylist.map((item) => {
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  return <CarouselItem key={item.id} {...item} />;
+                })}
+              </Carousel>
+            </Categories>
+          )}
 
-      <Categories title='Mi Lista'>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+          <Categories title='Tendencias'>
+            <Carousel>
+              {videos.trends.map((item) => {
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                return <CarouselItem key={item.id} {...item} />;
+              })}
+            </Carousel>
+          </Categories>
 
-      <Categories title='Tendencias'>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
-
-      <Categories title='Originales de Platzi Video'>
-        <Carousel>
-          <CarouselItem />
-        </Carousel>
-      </Categories>
-
+          <Categories title='Originales de Platzi Video'>
+            <Carousel>
+              {videos.originals.map((item) => {
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                return <CarouselItem key={item.id} {...item} />;
+              })}
+            </Carousel>
+          </Categories>
+        </React.Fragment>
+      )}
       <Footer />
     </div>
   );
---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+const useInitialState = (API) => {
+  const [videos, setVideos] = useState();
+
+  const getData = () => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => {
+        setVideos(data);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return videos;
+};
+
+export default useInitialState;
