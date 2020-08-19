22bb64f82c9e801190bf1298aaafab5202c8126d
danyel117 <dsaldarriaga@prevalentware.com>
2020-08-19 14:09:33 -0500

    configurar ESLINT y dar formatos

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import '../assets/styles/App.scss';
 
 const HelloWorld = () => {
-    return ( 
-        <h1>HelloWorld</h1>
-     );
-}
- 
+  return <h1>HelloWorld</h1>;
+};
+
 export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld';
 
-
-ReactDOM.render(<HelloWorld/>,document.getElementById("app"))
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
